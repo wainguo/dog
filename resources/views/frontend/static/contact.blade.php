ec0baf1ad9ddc54527bc8f54290484e368ad8948
guoshengxing <guoshengxing@renrendai.com>
2016-09-26 01:53:16 +0800

    2016.9.26

--- resources/views/frontend/static/contact.blade.php
+++ resources/views/frontend/static/contact.blade.php
@@ -12,24 +12,23 @@
         <h4>若有任何问题，请通过以下途径联系我们：</h4>
         <div class="ui piled segments">
             <div class="ui segment">
-                <p>商务合作：hezuo@jtmds.cn</p>
+                <p>商务合作：44862914#qq.com</p>
             </div>
             <div class="ui segment">
-                <p>改进建议：http://www.jtmds.cn/feedback</p>
+                <p>改进建议：44862914#qq.com</p>
             </div>
             <div class="ui segment">
-                <p>友情链接：http://www.jtmds.cn/links</p>
+                <p>友情链接：44862914#qq.com</p>
+                {{--<p>友情链接：http://www.jtmds.cn/links</p>--}}
             </div>
 
             <div class="ui segment">
-                <p>客服热线：service@jtmds.cn</p>
+                <p>客服热线：44862914#qq.com</p>
             </div>
             <div class="ui segment">
-                <p>投稿相关：service@jtmds.cn</p>
-            </div>
-            <div class="ui segment">
-                <p>友情链接：http://www.smzdm.com/links</p>
+                <p>投稿相关：44862914#qq.com</p>
             </div>
         </div>
+        <small>请将以上"#" 替换成 "@"</small>
     </div>
 @endsection
