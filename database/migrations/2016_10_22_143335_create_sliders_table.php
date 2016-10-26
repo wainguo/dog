2c867383a7639d1e32988589e58bbe9fbc7b9790
guoshengxing <guoshengxing@renrendai.com>
2016-10-26 21:03:30 +0800

    2016.10.26 slider manage

---
+++ database/migrations/2016_10_22_143335_create_sliders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 80);
+            $table->string('cover', 255);
+            $table->string('url', 255);
+            $table->tinyInteger('status')->default(1);
+            $table->integer('sort')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sliders');
+    }
+}
